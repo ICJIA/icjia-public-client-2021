6ab7e3d75cc6f4809b190816c0a67bc13d1430db
cschweda <cschweda@gmail.com>
2021-06-17 15:33:37 -0500

    fix: prep single article query

--- src/views/Sandbox2.vue
+++ src/views/Sandbox2.vue
@@ -1,19 +1,52 @@
 <template>
   <div>
-    {{ article }}
+    <v-container fluid>
+      <v-row v-if="loading">
+        <v-col cols="12">
+          <Loader loaderType="skeleton" :repeat="1"></Loader>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <div v-if="article">
+            {{ article }}
+          </div>
+          <div v-if="error" class="text-center error" style="font-size: 20px">
+            {{ error }}
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
 const axios = require("axios");
+import NProgress from "nprogress";
+const api = axios.create({
+  baseURL: "https://researchhub.icjia-api.cloud",
+  timeout: 15000,
+});
+
+api.interceptors.request.use((config) => {
+  NProgress.start();
+  return config;
+});
+
+api.interceptors.response.use((response) => {
+  NProgress.done();
+  return response;
+});
 export default {
   data() {
     return {
       article: null,
+      error: null,
+      loading: true,
       slug: "examining-the-experiences-of-women-police-leaders-in-illinois",
     };
   },
-  mounted() {
+  async mounted() {
     const query = `query {
   articles (where: { status: "published", slug: "${this.slug}" }) {
      id
@@ -22,6 +55,7 @@ export default {
       slug
       date
       external
+      thumbnail
       categories
       tags
       authors
@@ -48,14 +82,21 @@ export default {
   }
  
 }`;
-    axios
-      .create({ baseURL: "https://researchhub.icjia-api.cloud" })
-      .post("/graphql", { query, validateStatus: (status) => status === 200 })
-      .then((res) => {
-        console.log(res);
-        this.article = res.data.data.articles[0];
-      })
-      .catch((err) => console.error(err));
+    try {
+      let article = await api.post("/graphql", {
+        query,
+        validateStatus: function (status) {
+          return status >= 200 && status < 300;
+        },
+      });
+      this.article = article.data.data.articles[0];
+      this.loading = false;
+    } catch (e) {
+      console.log(e);
+      this.error = e;
+      NProgress.done();
+      this.loading = false;
+    }
   },
 };
 </script>
