6f697ec09ff732a24c05905a3d6151648bb6ffbd
cschweda <cschweda@gmail.com>
2021-06-30 16:52:11 -0500

    fix: grant pages

--- src/router/grants/index.js
+++ src/router/grants/index.js
@@ -18,6 +18,19 @@ const grants = [
       ),
   },
   {
+    path: "/grants/funding/",
+    // eslint-disable-next-line no-unused-vars
+    redirect: (route) => {
+      return "/grants/fsgu-funding/";
+    },
+  },
+  {
+    path: "/grants/fsgu-funding/",
+    name: "FundingAll",
+    component: () =>
+      import(/* webpackChunkName: "funding" */ "@/views/Grants/FundingAll.vue"),
+  },
+  {
     path: "/grants/ta/",
     // eslint-disable-next-line no-unused-vars
     redirect: (route) => {
