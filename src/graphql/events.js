86df97e5cdd5b5279dccc211bb12e53ab431a0d6
Chris Schweda <cschweda@gmail.com>
2021-07-07 09:59:30 -0500

    fix: scaffold out event calendar

---
+++ src/graphql/events.js
@@ -0,0 +1,41 @@
+import gql from "graphql-tag";
+
+const GET_EVENTS = gql`
+  query Events {
+    events(sort: "start:asc") {
+      id
+      created_at
+      updated_at
+      published_at
+      name
+      start
+      end
+      timed
+      summary
+      category
+      slug
+      details
+    }
+  }
+`;
+
+const GET_SINGLE_EVENT = gql`
+  query singleEvent($slug: String!) {
+    events(where: { slug: $slug }) {
+      id
+      created_at
+      updated_at
+      published_at
+      name
+      start
+      end
+      timed
+      summary
+      category
+      slug
+      details
+    }
+  }
+`;
+
+export { GET_EVENTS, GET_SINGLE_EVENT };
--- src/views/Events/EventsAll.vue
+++ src/views/Events/EventsAll.vue
@@ -1,11 +1,51 @@
 <template>
   <div>
-    <h1>ICJIA Event Calendar here</h1>
+    {{ filteredEvents }}
   </div>
 </template>
 
 <script>
-export default {};
-</script>
+/* eslint-disable no-unused-vars */
+import { GET_EVENTS } from "@/graphql/queries/events";
+import { fixButtonText } from "@/a11y";
 
-<style lang="scss" scoped></style>
+// import NProgress from "nprogress";
+const moment = require("moment");
+const tz = require("moment-timezone");
+import { EventBus } from "@/event-bus";
+export default {
+  apollo: {
+    events: {
+      query: GET_EVENTS,
+      variables() {},
+      error(error) {
+        this.error = JSON.stringify(error.message);
+      },
+      result(ApolloQueryResult) {
+        this.events = this.events.map((event) => {
+          event.start = moment(event.start)
+            .tz(this.$myApp.config.timezone)
+            .toDate();
+          event.end = moment(event.end)
+            .tz(this.$myApp.config.timezone)
+            .toDate();
+          //console.log(this.$myApp.config.events[event.type]["color"]);
+          if (this.$myApp.config.events[event.type]) {
+            event.color = this.$myApp.config.events[event.type]["color"];
+          } else {
+            event.color = "grey darken-4";
+          }
+          //event.color = this.colors[this.rnd(0, this.colors.length - 1)];
+          event.show = false;
+          return event;
+        });
+        //this.filteredEvents = this.events;
+
+        this.filterUpcoming();
+        this.isLoading = false;
+        window.NProgress.done();
+      },
+    },
+  },
+};
+</script>
