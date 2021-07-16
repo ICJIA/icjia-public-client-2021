e98d4fdd028d688fd2e247db6f74f52632ccdb2f
cschweda <cschweda@gmail.com>
2021-07-16 11:50:21 -0500

    fix: add drone reporting and infonet

--- src/router/informationSystems/index.js
+++ src/router/informationSystems/index.js
@@ -1,5 +1,12 @@
 const informationSystems = [
   {
+    path: "/drone/",
+    // eslint-disable-next-line no-unused-vars
+    redirect: (route) => {
+      return "/information-systems/drone/";
+    },
+  },
+  {
     path: "/information-systems/",
     name: "ISUOvervierw",
     component: () =>
@@ -7,10 +14,27 @@ const informationSystems = [
         /* webpackChunkName: "isu" */ "@/views/InformationSystems/ISUHome.vue"
       ),
   },
+  {
+    path: "/information-systems/infonet",
+    name: "Infonet",
+    component: () =>
+      import(
+        /* webpackChunkName: "isu" */ "@/views/InformationSystems/Infonet.vue"
+      ),
+  },
+
+  {
+    path: "/information-systems/isu-staff",
+    name: "ISUStaff",
+    component: () =>
+      import(
+        /* webpackChunkName: "isu" */ "@/views/InformationSystems/ISUStaff.vue"
+      ),
+  },
 
   {
     path: "/information-systems/:slug",
-    name: "ISUpage",
+    name: "ISUBasepage",
     component: () =>
       import(/* webpackChunkName: "isu" */ "@/views/BasePage.vue"),
   },
---
+++ src/views/InformationSystems/ISUStaff.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>ISU Staff Listing here</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
