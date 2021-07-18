fbde0f84a291eb6c05336dbccf38c6e58ff4001b
cschweda <cschweda@gmail.com>
2021-07-18 16:53:36 -0500

    fix: refactor contextTabs for addition of more tabs

--- src/views/Sandbox/Sandbox.vue
+++ src/views/Sandbox/Sandbox.vue
@@ -1,95 +1,165 @@
 <template>
-  <div>
-    <div
-      class="pl-3 pr-9 py-3"
-      style="
-        background: #0d4474;
-        color: #fff;
-
-        font-size: 18px;
-      "
-      :class="{
-        'text-left':
-          $vuetify.breakpoint.md ||
-          $vuetify.breakpoint.lg ||
-          $vuetify.breakpoint.xl,
-        'text-center': $vuetify.breakpoint.sm || $vuetify.breakpoint.xs,
-      }"
+  <div id="contextBar">
+    <v-app-bar
+      height="45"
+      scroll-threshold="0"
+      color="#0a3a60"
+      elevate-on-scroll
+      v-resize="resize"
     >
-      <span>
-        <span style="font-weight: 600">ICJIA Overview</span>
-        &raquo;
-        <span style="font-weight: 300">{{
-          currentItem.replace("tab-", "")
-        }}</span>
-      </span>
-    </div>
-    <v-toolbar dense v-resize="onResize" color="#105a96" dark flat>
       <v-tabs
-        v-model="currentItem"
-        fixed-tabs
-        slider-color="white"
+        dark
         show-arrows
-        dense
+        centered
+        v-model="contextTab"
+        center-active
+        height="45"
+        optional
+        class="context"
       >
-        <v-tab
-          v-for="(item, index) in tabs"
-          :key="`context${index}`"
-          :to="'#' + item"
-        >
-          {{ item }}
-        </v-tab>
+        <v-tabs-slider color="white"></v-tabs-slider>
 
+        <v-tab
+          style="background: #0a3a60 !important; font-size: 12px"
+          v-for="(item, index) in contextMenu[0].items"
+          :key="index"
+        >
+          {{ item.label }}
+          <v-icon v-if="item.icon" right small>{{ item.icon }}</v-icon>
+        </v-tab>
         <v-menu v-if="more.length" bottom left>
-          <template v-slot:activator="{ on }">
-            <v-btn text class="align-self-center mr-4" v-on="on">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn text class="align-self-center mr-4" v-bind="attrs" v-on="on">
               more
-              <v-icon right>mdi-menu-down</v-icon>
+              <v-icon right> mdi-menu-down </v-icon>
             </v-btn>
           </template>
 
           <v-list class="grey lighten-3">
-            <v-list-item
-              v-for="item in more"
-              :key="`more${item}`"
-              @click="addItem(item)"
-            >
+            <v-list-item v-for="item in more" :key="item">
               {{ item }}
             </v-list-item>
           </v-list>
         </v-menu>
       </v-tabs>
-    </v-toolbar>
+    </v-app-bar>
   </div>
 </template>
 
 <script>
+import { EventBus } from "@/event-bus";
 export default {
-  data() {
-    return {
-      currentItem: "Shopping",
-      items: ["Web", "Shopping", "Videos", "Images", "Tab 5", "Tab 6"],
-      tabs: [],
-      more: [],
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    };
+  // props: {
+  //   contextMenu: {
+  //     type: Array,
+  //     default: () => [],
+  //   },
+  // },
+  watch: {
+    // eslint-disable-next-line no-unused-vars
+    contextTab(newValue, oldValue) {},
+  },
+  mounted() {
+    //console.log(this.contextMenu[0].items);
+
+    this.selectTab();
   },
   methods: {
-    addItem(item) {
-      const removed = this.tabs.splice(this.tabs.length - 1, 1);
-      this.tabs.push(...this.more.splice(this.more.indexOf(item), 1));
-      this.more.push(...removed);
-      this.$nextTick(() => {
-        this.currentItem = item;
+    selectTab() {
+      this.contextMenu[0].items.forEach((item, index) => {
+        if (this.$route.fullPath === item.path) {
+          this.contextTab = index;
+        }
       });
     },
-    onResize() {
-      const temp = this.items.slice();
-      this.tabs = temp.splice(0, window.innerWidth / 120 - 1);
-      this.more = temp.splice(0);
+    fireEvent() {
+      EventBus.$emit("search");
+      this.$nextTick(() => {
+        this.contextTab = undefined;
+        this.selectTab();
+      });
     },
+    routeToPage(page) {
+      //   if (page === "About the Research Hub") return;
+      // console.log("route: ", page);
+      this.$router.push(page).catch(() => {
+        this.$vuetify.goTo(0);
+      });
+    },
+  },
+  data() {
+    return {
+      contextDrawer: true,
+      contextTab: null,
+      isAtTop: false,
+      disabled: false,
+      more: [],
+      contextMenu: [
+        {
+          name: "newsAndInfo",
+          label: "News & Information",
+          shortLabel: "News & Info",
+          pathPrefix: "/news/",
+          defaultPath: "/news/",
+          items: [
+            {
+              label: "Latest News",
+              path: "/news/",
+            },
+            {
+              label: "Meetings",
+              path: "/meetings/",
+            },
+            {
+              label: "Funding",
+              path: "/grants/funding/",
+            },
+            {
+              label: "Employment",
+              path: "/employment/",
+            },
+            {
+              label: "Events",
+              path: "/events/",
+            },
+            {
+              label: "MeetingsA",
+              path: "/meetings/",
+            },
+            {
+              label: "FundingA",
+              path: "/grants/funding/",
+            },
+            {
+              label: "EmploymentA",
+              path: "/employment/",
+            },
+            {
+              label: "EventsB",
+              path: "/events/",
+            },
+            {
+              label: "MeetingsB",
+              path: "/meetings/",
+            },
+            {
+              label: "FundingB",
+              path: "/grants/funding/",
+            },
+            {
+              label: "EmploymentB",
+              path: "/employment/",
+            },
+            {
+              label: "EventsB",
+              path: "/events/",
+            },
+          ],
+        },
+      ],
+    };
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss"></style>
