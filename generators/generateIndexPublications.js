0d2808ced5f425702dd60e5f747d0e63c3ee46ba
cschweda <cschweda@gmail.com>
2021-08-30 10:00:31 -0500

    fix: add loop of queries to publication generator

--- generators/generateIndexPublications.js
+++ generators/generateIndexPublications.js
@@ -6,56 +6,55 @@ const _ = require("lodash");
 // const { apiBaseURL } = require("./src/config");
 const allowedHost = "https://icjia.illinois.gov/researchhub";
 
-const query = `query {
-  publications(limit: 990, sort: "published_at:desc") {
-      id
-      published_at
-      publicationDate
-      title
-      slug
-      summary
-      fileURL
-      articleURL
-      datasetURL
-      applicationURL
-      pubType
-      tags
-      searchMeta
+const PUBLICATIONS_COUNT = `query {
+    publicationsConnection {
+    aggregate {
+      totalCount
     }
-}`;
+  }
+  }
+`;
 
-axios
-  .create({ baseURL: "https://agency.icjia-api.cloud" })
-  .post("/graphql", { query, validateStatus: (status) => status === 200 })
-  .then((res) => {
-    let publications = res.data.data.publications;
-    publications = publications.map((p) => {
-      let obj = {
-        ...p,
-        altTitle: p.title.toLowerCase(),
-        localArticlePath:
-          p.articleURL && p.articleURL.includes(allowedHost)
-            ? p.articleURL.replace("https://icjia.illinois.gov", "")
-            : null,
-        fullPath: `/about/publications/${p.slug}`,
-        contentType: "publication",
-      };
-      return obj;
-    });
+const init = async () => {
+  const limit = 500;
+  let pubArray = [];
+  let start = 0;
+  let count = await axios.get(
+    "https://agency.icjia-api.cloud/publications/count"
+  );
+  count = count.data;
+  let iterations = Math.ceil(count / limit);
 
-    let content = [...publications];
-    content = _.orderBy(content, ["publicationDate"], ["desc"]);
-
-    const dirpath = "./public/api";
-    if (!fs.existsSync(dirpath)) fs.mkdirSync(dirpath);
-
-    jsonfile.writeFile(
-      `./public/api/publications.json`,
-      content,
-      function (err) {
-        if (err) console.error(err);
-        console.log(`Created: ./public/api/publications.json`);
-      }
+  for (let i = 0; i < iterations; i++) {
+    let response = await axios.get(
+      `https://agency.icjia-api.cloud/publications?_limit=${limit}&_start=${start}`
     );
-  })
-  .catch((err) => console.error(err));
+    pubArray = pubArray.concat(response.data);
+    start += limit;
+  }
+  pubArray = _.uniqBy(pubArray, "id");
+  let publications = pubArray.map((p) => {
+    let obj = {
+      ...p,
+      altTitle: p.title.toLowerCase(),
+      localArticlePath:
+        p.articleURL && p.articleURL.includes(allowedHost)
+          ? p.articleURL.replace("https://icjia.illinois.gov", "")
+          : null,
+      fullPath: `/about/publications/${p.slug}`,
+      contentType: "publication",
+    };
+    return obj;
+  });
+  let content = [...publications];
+  content = _.orderBy(content, ["publicationDate"], ["desc"]);
+
+  const dirpath = "./public/api";
+  if (!fs.existsSync(dirpath)) fs.mkdirSync(dirpath);
+  jsonfile.writeFile(`./public/api/publications.json`, content, function (err) {
+    if (err) console.error(err);
+    console.log(`Created: ./public/api/publications.json`);
+  });
+};
+
+init();
