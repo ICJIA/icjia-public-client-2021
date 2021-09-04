3b16851b3c71ffce541eeb6a0b7682c0e44140d2
cschweda <cschweda@gmail.com>
2021-09-04 15:19:38 -0500

    fix: combine searchIndex and sitemap generation

--- generators/generateIndexAndSitemap.js
+++ generators/generateIndexAndSitemap.js
@@ -4,6 +4,10 @@ const axios = require("axios");
 const jsonfile = require("jsonfile");
 const _ = require("lodash");
 
+const config = require("../src/config/config.json");
+const { createWriteStream } = require("fs");
+const { SitemapStream } = require("sitemap");
+
 const hub = require("../public/api/hub.json");
 const grants = require("../public/api/grants.json");
 const pages = require("../public/api/pages.json");
@@ -28,7 +32,6 @@ let siteIndex = [
   ...events,
 ];
 
-// siteIndex = _.orderBy(siteIndex, ["date"], ["desc"]);
 const dirpath = "./public/api";
 if (!fs.existsSync(dirpath)) fs.mkdirSync(dirpath);
 console.log("Site index length: ", siteIndex.length);
@@ -36,3 +39,19 @@ jsonfile.writeFile(`./public/searchIndex.json`, siteIndex, function (err) {
   if (err) console.error(err);
   console.log(`Created: ./public/searchIndex.json`);
 });
+
+// Create Sitemap.xmp here
+
+const sitemap = new SitemapStream({ hostname: `${config.api.baseClient}` });
+const writeStream = createWriteStream("./public/sitemap.xml");
+sitemap.pipe(writeStream);
+
+siteIndex.forEach((item) => {
+  let url = `${config.api.baseClient}${item.fullPath}`;
+  url += url.endsWith("/") ? "" : "/";
+  sitemap.write({ url, changefreq: "weekly", priority: 0.3 });
+});
+
+sitemap.end();
+
+console.log("Created: ./public/Sitemap.xml");
