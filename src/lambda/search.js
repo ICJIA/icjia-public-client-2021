521dfa950aaa0ba2999503c1dff33c90e084d706
cschweda <cschweda@gmail.com>
2021-09-06 14:06:26 -0500

    fix: lambda

--- src/lambda/search.js
+++ src/lambda/search.js
@@ -3,12 +3,17 @@ const fs = require("fs").promises;
 const path = require("path");
 
 export async function handler(event, context) {
-  const content = await fs.readFile("./public/data.json", {
+  const content = await fs.readFile(path.join("data.json"), {
     encoding: "utf-8",
   });
-
+  // const content = await fs.readFile(path.resolve("./data.json"), {
+  //   encoding: "utf-8",
+  // });
+  console.log(path.join("data.json"));
   return {
     statusCode: 200,
-    body: content,
+    body: JSON.stringify({
+      message: `Hello world ${Math.floor(Math.random() * 10)}`,
+    }),
   };
 }
