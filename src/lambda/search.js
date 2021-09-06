6c64b42dd54036ff3ece1be080f18f51f72dca62
cschweda <cschweda@gmail.com>
2021-09-06 13:15:42 -0500

    fix: lambda

--- src/lambda/search.js
+++ src/lambda/search.js
@@ -3,12 +3,12 @@ const fs = require("fs").promises;
 const path = require("path");
 
 export async function handler(event, context) {
-  //   const content = await fs.readFile(path.join(__dirname, "data.json"), {
-  //     encoding: "utf-8",
-  //   });
-  const content = await fs.readFile(path.resolve("./dist/searchIndex.json"), {
+  const content = await fs.readFile(path.join(__dirname, "data.json"), {
     encoding: "utf-8",
   });
+  // const content = await fs.readFile(path.resolve("./dist/searchIndex.json"), {
+  //   encoding: "utf-8",
+  // });
 
   return {
     statusCode: 200,
