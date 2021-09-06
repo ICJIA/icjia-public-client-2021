fd3221819abc34746e04825a9529eea41b92a65a
cschweda <cschweda@gmail.com>
2021-09-06 14:23:22 -0500

    fix: json path

--- src/lambda/search.js
+++ src/lambda/search.js
@@ -13,7 +13,7 @@ export async function handler(event, context) {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Hello world ${Math.floor(Math.random() * 10)}`,
+      message: content,
     }),
   };
 }
