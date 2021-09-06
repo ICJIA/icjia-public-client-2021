617aa925c10bfb87cb12caee97c3e66978a02b15
cschweda <cschweda@gmail.com>
2021-09-06 16:52:49 -0500

    fix: add cors headers directly to lambda

--- src/lambda/search.js
+++ src/lambda/search.js
@@ -2,12 +2,25 @@
 const fs = require("fs").promises;
 const path = require("path");
 const content = require("./searchIndex.json");
+let HEADERS = {
+  "Access-Control-Allow-Headers":
+    "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin",
+  "Content-Type": "application/json", //optional
+  "Access-Control-Allow-Methods": "POST, OPTIONS",
+  "Access-Control-Max-Age": "8640",
+};
+
+//This solves the "No ‘Access-Control-Allow-Origin’ header is present on the requested resource."
+
+HEADERS["Access-Control-Allow-Origin"] = "*";
+HEADERS["Vary"] = "Origin";
 export async function handler(event, context) {
   //console.log(path.join("data.json"));
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: content,
+      HEADERS,
     }),
   };
 }
