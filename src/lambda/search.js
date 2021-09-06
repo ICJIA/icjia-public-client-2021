533cb752e407682e0dfc0eb6676bf7f2d639238c
cschweda <cschweda@gmail.com>
2021-09-06 13:27:46 -0500

    fix: local data load in function

--- src/lambda/search.js
+++ src/lambda/search.js
@@ -3,12 +3,9 @@ const fs = require("fs").promises;
 const path = require("path");
 
 export async function handler(event, context) {
-  const content = await fs.readFile(path.join(__dirname, "data.json"), {
+  const content = await fs.readFile("data.json", {
     encoding: "utf-8",
   });
-  // const content = await fs.readFile(path.resolve("./dist/searchIndex.json"), {
-  //   encoding: "utf-8",
-  // });
 
   return {
     statusCode: 200,
