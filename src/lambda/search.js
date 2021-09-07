3653f33992248efa83a76b73449acda17039ba8c
cschweda <cschweda@gmail.com>
2021-09-07 07:03:06 -0500

    fix: add access control

--- src/lambda/search.js
+++ src/lambda/search.js
@@ -6,7 +6,7 @@ let HEADERS = {
   "Access-Control-Allow-Headers":
     "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin",
   "Content-Type": "application/json", //optional
-  "Access-Control-Allow-Methods": "POST, OPTIONS",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
   "Access-Control-Max-Age": "8640",
 };
 
