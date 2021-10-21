73f629d91c00683f1b839f4a3d02ee7a5ea5d3a2
cschweda <cschweda@gmail.com>
2021-10-21 08:38:25 -0500

    fix: serverless

--- src/components/ModalSearch.vue
+++ src/components/ModalSearch.vue
@@ -83,7 +83,7 @@ export default {
     if (process.env.NODE_ENV === "development") {
       searchURL = "/.netlify/functions/search";
     } else {
-      searchURL = `${this.$myApp.config.netlifyURL}/.netlify/functions/search`;
+      searchURL = `https://icjia-public.netlify.app/.netlify/functions/search`;
     }
     let response = await fetch(searchURL);
     if (!response.ok) {
