7529233ddb04e6e83f51bb6b983e2a4adaad3d20
cschweda <cschweda@gmail.com>
2021-10-21 08:44:19 -0500

    fix: cors in serverless

--- src/lambda/search.js
+++ src/lambda/search.js
@@ -3,17 +3,15 @@ const fs = require("fs").promises;
 const path = require("path");
 const content = require("./searchIndex.json");
 let HEADERS = {
-  "Access-Control-Allow-Headers":
-    "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin",
-  "Content-Type": "application/json", //optional
-  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-  "Access-Control-Max-Age": "8640",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
 };
 
-//This solves the "No ‘Access-Control-Allow-Origin’ header is present on the requested resource."
+// //This solves the "No ‘Access-Control-Allow-Origin’ header is present on the requested resource."
 
-HEADERS["Access-Control-Allow-Origin"] = "*";
-HEADERS["Vary"] = "Origin";
+// HEADERS["Access-Control-Allow-Origin"] = "*";
+// HEADERS["Vary"] = "Origin";
 export async function handler(event, context) {
   //console.log(path.join("data.json"));
   return {
