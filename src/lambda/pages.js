3a60469864fdaa969eeed9bb224c8df2dbf8cff9
cschweda <cschweda@gmail.com>
2021-10-29 14:53:12 -0500

    feat: add lambda for pageviews via plausible api

---
+++ src/lambda/pages.js
@@ -0,0 +1,50 @@
+const axios = require("axios");
+require("dotenv").config();
+const moment = require("moment");
+// eslint-disable-next-line no-unused-vars
+const tz = require("moment-timezone");
+const tstamp = moment();
+const chicagoTime = tstamp
+  .tz("America/Chicago")
+  .format("dddd, MMMM Do YYYY, h:mm:ssa z");
+
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Methods": "GET",
+};
+
+const limit = 15;
+
+exports.handler = (event, context, callback) => {
+  let period = event.queryStringParameters.period;
+  if (!period) {
+    period = "day";
+  }
+  console.log(period);
+  axios
+    .get(
+      `https://analytics.metaincognita.com/api/v1/stats/breakdown?site_id=icjia.illinois.gov&period=${period}&property=event:page&limit=${limit}`,
+      {
+        headers: {
+          "cache-control": "no-cache",
+          Authorization: `Bearer ${process.env.PLAUSIBLE_API_KEY}`,
+        },
+      }
+    )
+    .then((res) => {
+      let jsonResponse = {
+        results: res.data.results,
+        period: period,
+        date: chicagoTime,
+      };
+      callback(null, {
+        statusCode: 200,
+        headers,
+        body: JSON.stringify(jsonResponse),
+      });
+    })
+    .catch((err) => {
+      callback(err);
+    });
+};
