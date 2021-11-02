cbd4636c7fc306e15fb98cf26a722f67a4eb497e
cschweda <cschweda@gmail.com>
2021-11-02 08:08:50 -0500

    fix: add direct hub downlaods to analytics

--- src/views/Hub/DatasetsSingle.vue
+++ src/views/Hub/DatasetsSingle.vue
@@ -98,6 +98,8 @@ export default {
     },
     async downloader() {
       const { hash, ext } = this.item.datafile;
+      let analyticsURL = `/uploads/${hash}${ext}`;
+      window.plausible("file_download", { props: { url: analyticsURL } });
       window.open(
         `https://researchhub.icjia-api.cloud/uploads/${hash}${ext}`,
         "_blank"
