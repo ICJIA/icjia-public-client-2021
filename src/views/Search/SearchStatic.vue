7f9f18378ca400ee7165aab91788edd3a2b99aba
cschweda <cschweda@gmail.com>
2022-04-19 11:00:14 -0500

    fix: add search query option

--- src/views/Search/SearchStatic.vue
+++ src/views/Search/SearchStatic.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>Static Search/Tag search here</div>
+  <div>
+    <h1>Static Search/Tag search here</h1>
+    <div v-if="$route.params.query">
+      <h2>Search query: {{ $route.params.query }}</h2>
+    </div>
+  </div>
 </template>
 
 <script>
